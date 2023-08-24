43397b92d669a3a184f049f07992cdfb31c77420
Riley Shipman <108430567+Cal-V@users.noreply.github.com>
2023-08-23 18:40:47 -0600

    nav bar

---
+++ pixel-art/src/components/Layout/Nav.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+function Nav({user}) {
+
+    if (user) {
+        return (
+            <nav>
+                <button>Save Canvas</button>
+                <button>Save Pattern</button>
+                <button>My Patterns</button>
+                <button>My Canvases</button>
+                <button>Log out</button>
+            </nav>
+        )
+    }
+
+    return (
+        <nav>
+            <button>Login</button>
+            <button>Export Canvas</button>
+            <button>Export Pattern</button>
+        </nav>
+    )
+}
+
+export default Nav
