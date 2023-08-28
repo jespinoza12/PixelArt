f44a69927f241961363679ba4f68b57a390c4806
Riley Shipman <108430567+Cal-V@users.noreply.github.com>
2023-08-28 09:47:22 -0600

    added route

--- pixel-art/src/components/App.js
+++ pixel-art/src/components/App.js
@@ -4,6 +4,7 @@ import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
+import PatternReader from "./PatternReader";
 
 function App() {
 
@@ -11,26 +12,10 @@ function App() {
     {
       path: "/",
       element:<Editor />,
-    },
-    // {
-    //   path: "/pattern/c2c",
-    //   element:<Pattern />,
-    // },{
-    //   path: "/pattern/c2c-inverse",
-    //   element:<Pattern />,
-    // },{
-    //   path: "/pattern/sc-up",
-    //   element:<Pattern />,
-    // },{
-    //   path: "/pattern/sc-down",
-    //   element:<Pattern />,
-    // },{
-    //   path: "/pattern/sc-right",
-    //   element:<Pattern />,
-    // },{
-    //   path: "/pattern/sc-left",
-    //   element:<Pattern />,
-    // }
+    },{
+      path: "pattern",
+      element: <PatternReader />
+    }
   ]);
 
   return (
